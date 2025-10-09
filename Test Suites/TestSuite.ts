e6416d09a2e99a41a846e85e0d5423cf5a98abac
10835442 <10835442@7DS3S93>
2025-10-09 11:37:51 +0530

    two suites changed and two cases should run

--- Test Suites/TestSuite.ts
+++ Test Suites/TestSuite.ts
@@ -12,25 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>2e1776aa-e5de-414d-90ea-b9de75ceee13</testSuiteGuid>
    <testCaseLink>
-      <guid>bf7c0e35-1199-4819-87b6-f22bb13b5175</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_SignIn</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>9abffa8f-76bf-4598-ab80-77ab1289486d</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>b0c0380f-1bcd-4edd-9132-890379df2fb0</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
       <guid>a4984d5c-c3d8-49d1-b9e2-423ae2eeabfc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
