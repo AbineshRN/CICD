aa150625485d6a9992164ef96291270c5642d554
10835442 <10835442@7DS3S93>
2025-10-10 07:24:17 +0530

    suite

--- Test Suites/RegisterSignIN.ts
+++ Test Suites/RegisterSignIN.ts
@@ -30,4 +30,23 @@
          <variableId>b0c0380f-1bcd-4edd-9132-890379df2fb0</variableId>
       </variableLink>
    </testCaseLink>
+   <testCaseLink>
+      <guid>6483bade-1d73-486e-a446-a359815e1fb0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Blog_PostComment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6967210b-ef28-48be-a4c6-5c55ab3fcf1e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d2d6dc56-1d16-4837-8119-8cc1b0c1698c</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
